da6e56d0ddd13a292e3a357cfea800986bfdee12
omkale1719 <omk28035@gmail.com>
2024-11-09 10:48:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -305,6 +305,6 @@ app.get("/Contact_Us", (req, res) => {
 // Start server on port 3000
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
